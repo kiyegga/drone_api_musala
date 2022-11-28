88fc1d40f3def0b2691d1c4522e0a052c3643b7c
FRANCIS.KIYEGGA <francis.kiyegga@equitybank.co.ug>
2022-11-28 21:50:04 +0300

    updates in application property file, addition of some classes/entities

--- src/main/java/com/example/droneAPI/model/DroneBatteryLevel.java
+++ src/main/java/com/example/droneAPI/model/DroneBatteryLevel.java
@@ -1,12 +1,10 @@
 package com.example.droneAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jdk.jfr.Timestamp;
 import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -16,5 +14,10 @@ public class DroneBatteryLevel {
     private  Long Id;
     private String serialNumber;
     private double batteryLevel;
-    private Timestamp timestamp;
+    private LocalDateTime timestamp;
+
+  //many to one relationship with drone table
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "droneId", referencedColumnName = "Id")
+    private Drone drone;
 }
